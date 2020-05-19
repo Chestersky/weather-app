d6aa3c3ffd2517be4e40c47f698586b92605d3e1
Tomasz Szkaradek <tomasz.szkrdk@gmail.com>
2020-05-19 15:51:15 +0200

    delete Container

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import FiveDayWeather from "./containers/FiveDayWeather/FiveDayWeather";
 import Weather from "./containers/Weather/Weather";
 import Toolbar from "./components/Toolbar/Toolbar";
-import Container from "react-bootstrap/Container";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Container>
+        <div>
           <Toolbar />
           <Switch>
             <Route path="/" exact component={Weather} />
             <Route path="/fiveday" component={FiveDayWeather} />
           </Switch>
-        </Container>
+        </div>
       </BrowserRouter>
     );
   }
