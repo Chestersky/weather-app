840947242d651db43c8e4f97d13346399a600186
Tomasz Szkaradek <tomasz.szkrdk@gmail.com>
2020-05-28 17:22:01 +0200

    change weather icons

--- src/components/DayItem/DayItem.js
+++ src/components/DayItem/DayItem.js
@@ -2,19 +2,14 @@ import React from "react";
 import moment from "moment";
 import "moment/locale/pl";
 import "./DayItem.css";
+import "../../weather-icons/css/weather-icons.css";
 
 const dayitem = (props) => {
   return (
     <div className="column">
       <h4>{moment(props.data.Date).locale("pl").format("dddd")}</h4>
       <div className="icon">
-        <img
-          alt="cos"
-          src={
-            "https://openweathermap.org/img/wn/" +
-            props.icon[props.data.Day.Icon]
-          }
-        />
+        <i className={"wi " + props.icon[props.data.Day.Icon]} />
         <p className="lead">{props.data.Day.IconPhrase}</p>
       </div>
       <div className="temp">
